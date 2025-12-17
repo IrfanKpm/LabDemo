16ebfdce4aa41e586976f0536c9c479ed9a60072
MUHAMMED IRFAN P <112098500+IrfanKpm@users.noreply.github.com>
2025-12-17 07:17:45 +0530

    Remove import of LabInventorySystem component

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import LabInventorySystem from './LabInventorySystem.jsx'
 import LabInventorySystemDemo from './Demo.jsx'
 
 
